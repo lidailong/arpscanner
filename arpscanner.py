d8437c7a230438c3a930abd9b48458c85f53a00b
root <root@localhost.localdomain>
2014-03-04 17:00:37 +0800

    update

--- arpscanner.py
+++ arpscanner.py
@@ -2,9 +2,16 @@
 
 from scapy.all import *
 
+
+
+a = IP(dst="8.8.8.8")
+ip = a.src
+print "local ip is %s" %ip
+subnet = ip[:ip.rfind(".")+1]
+
 total = 0
 for i in range(1, 255):
-	ip = "192.168.40." + str(i)
+	ip = subnet + str(i)
 	arpRequest = Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip, hwdst="ff:ff:ff:ff:ff:ff")
 	arpResponse = srp1(arpRequest, timeout=0.01, verbose=0)
 	if arpResponse:
